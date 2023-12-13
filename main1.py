86333b034743b97780fd63ccc190aafda0575bf3
pauloaclara <153681709+pauloaclara@users.noreply.github.com>
2023-12-13 00:05:36 +0000

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,2 +1,21 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+from machine import Pin, Timer, RTC
 
+ledOnBoard = Pin("LED", Pin.OUT)
+ledOnBoard.on()
 
+firmware_url = "https://raw.githubusercontent.com/pauloaclara/testeOTA/main/"
+
+while True:
+    ledOnBoard.on()
+  pause
+    try:
+        ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main1.py")
+        ota_updater.download_and_install_update_if_available()
+        if ledOnBoard.on():
+            ledOnBoard.off()
+        else:
+            ledOnBoard.off()
+    except:
+        ledOnBoard.off()
