d0c24f3bbb6f83f2be292b62a1b8ffb649e542c0
pauloaclara <153681709+pauloaclara@users.noreply.github.com>
2023-12-13 17:25:34 +0000

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,20 +1,33 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 from machine import Pin, Timer, RTC
+import utime
+import time
 
 ledOnBoard = Pin("LED", Pin.OUT)
 ledOnBoard.on()
-
+#time=Timer()
 firmware_url = "https://raw.githubusercontent.com/pauloaclara/testeOTA/main/"
 
-#while True:
-    ledOnBoard.on()
-    #try:
+while True:
+    print("slpeeping")
+    time.sleep(2)
+
+    for i in range(10):
+        ledOnBoard.on()
+        time.sleep(0.5)
+        ledOnBoard.off()
+        time.sleep(0.5)
+    time.sleep(0.5)
+    try:
         ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main1.py")
         ota_updater.download_and_install_update_if_available()
-        if ledOnBoard.on():
+        for i in range(10):
+            ledOnBoard.on()
+            time.sleep(0.1)
             ledOnBoard.off()
-        else:
-            ledOnBoard.off()
-    #except:
+            time.sleep(0.1)
+    
+    except:
         ledOnBoard.on()
+        time.sleep(1)
