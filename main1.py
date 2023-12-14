474d2b480193df19755f12e75e08b8a4090eff27
pauloaclara <153681709+pauloaclara@users.noreply.github.com>
2023-12-14 22:36:42 +0000

    Update main1.py

--- main1.py
+++ main1.py
@@ -27,13 +27,13 @@ while True:
             time.sleep(0.1)
             ledOnBoard.off()
             time.sleep(0.1)
-    '''
+    
     for i in range(30):
         ledOnBoard.on()
         time.sleep(0.03)
         ledOnBoard.off()
         time.sleep(0.03)
-    #time.sleep(0.5)'''
+    #time.sleep(0.5)
     try:
         ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main1.py")
         ota_updater.download_and_install_update_if_available()
